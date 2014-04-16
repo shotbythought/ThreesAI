1b8b31a93ca457d108e3f91b9b21524affaa99b0
Michael Zeng <mzeng@imsa.edu>
2014-04-16 15:05:07 -0500

    Added Interface to project

---
+++ src/Simulator.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+/**
+ * Created by Student on 4/16/2014.
+ */
+public class Simulator {
+    public Simulator()
+    {
+
+    }
+
+    //CHANGE THESE
+    public void moveLeft()
+    {
+
+    }
+
+    public void moveRight()
+    {
+
+    }
+
+    public void moveUp()
+    {
+
+    }
+
+    public void moveDown()
+    {
+
+    }
+
+    public ArrayList<ArrayList<Integer>> getThreesGrid()
+    {
+
+    }
+}
